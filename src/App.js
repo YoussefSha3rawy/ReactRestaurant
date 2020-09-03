8df90e1ac681b92433ed9d359ee6e641f2d8757d
Youssef Shaarawy <youssefshaarawy@gmail.com>
2020-09-04 00:05:34 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Main from './components/Maincomponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
