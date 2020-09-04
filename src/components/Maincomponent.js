9d16071ef2f2ee934426b01975af850011f9afaa
Youssef Shaarawy <youssefshaarawy@gmail.com>
2020-09-04 03:42:43 +0200

    Aboutus page added

--- src/components/Maincomponent.js
+++ src/components/Maincomponent.js
@@ -10,6 +10,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -24,6 +25,12 @@ class Main extends Component {
     }
 
     render() {
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        };
         const HomePage = () => {
             return (
                 <Home
@@ -39,7 +46,9 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={()=> <About leaders={this.state.leaders}/>} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
